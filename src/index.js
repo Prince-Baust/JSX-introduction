c6e698eef7b2957457908debb751abd72beb6841
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-10-02 01:07:12 +0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Bismillah</h1>
+    <p>in sha Allah</p>
+  </div>,
+  document.querySelector("#root")
+);
